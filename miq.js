b2b445d53f93490396f0776420ea7d744427b0b3
MattG-ZA <gleedmatt@gmail.com>
2018-05-31 20:12:52 +0200

    ROT13 cipher: Added first draft. Also added Utility functions.

--- miq.js
+++ miq.js
@@ -26,7 +26,33 @@
            }
 
            return encryptedMessage;
+        },
+        rot13: function(message) {
+            let shiftedAlphabet = shiftAlphabet(13);
+            let encryptedMessage = '';
+
+            for (let i = 0; i < message.length; i++) {
+                let indexOfChar = alphabet.indexOf(message[i].toUpperCase());
+                let encryptedChar = indexOfChar > -1 ? shiftedAlphabet[indexOfChar] : message[i].toUpperCase();
+ 
+                encryptedMessage += encryptedChar;
+            }
+ 
+            return encryptedMessage;
         }
+    };
+
+    // Utility functions
+    function shiftAlphabet(number) {
+        let shiftedAlphabet = '';
+
+        for (let i = 0; i < alphabet.length; i++) {
+            let shiftedChar = alphabet[(i + number) % 26];
+
+            shiftedAlphabet += shiftedChar;
+        }
+
+        return shiftedAlphabet;
     };
 
     miq.init.prototype = miq.prototype;
