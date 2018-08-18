98fa48e1db941d614a1a00f1fa7b704181339e69
MattG-ZA <gleedmatt@gmail.com>
2018-08-18 22:14:33 +0200

    More styling updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,8 +12,8 @@ const encryptMessage = (cipher) => {
             encryptedMessage = m.atbash(message);
             document.getElementById('atbash_message_encrypted').value = encryptedMessage;
             
-            document.getElementById('atbash_alpha').innerHTML = boldCharacters(message, alphabet);
-            document.getElementById('atbash_alpha_reversed').innerHTML = boldCharacters(encryptedMessage, alphabet.split('').reverse().join(''));
+            document.getElementById('atbash_alpha').innerHTML = boldCharacters(message, alphabet, '#1e6bb8');
+            document.getElementById('atbash_alpha_reversed').innerHTML = boldCharacters(encryptedMessage, alphabet.split('').reverse().join(''), '#ef4e7b');
 
             break;
         default:
@@ -22,17 +22,17 @@ const encryptMessage = (cipher) => {
 };
 
 // Utility Functions
-const boldCharacters = (message, alphabet) => {
+const boldCharacters = (message, alphabet, colour) => {
     let boldAlphabet = '';
 
     for(let i = 0; i < alphabet.length; i++) {
         if (message.toUpperCase().indexOf(alphabet[i]) > -1) {
-            boldAlphabet += '<span style="color: #1e6bb8"><b>' + alphabet[i] + '</b></span>';
+            boldAlphabet += `<span style="color: ${colour}"><b>` + alphabet[i] + '</b></span>';
         }
         else {
             boldAlphabet += alphabet[i];
         }
     }
-
+    console.log(boldAlphabet);
     return boldAlphabet;
 }
